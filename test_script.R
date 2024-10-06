6a04ef7aa21a650418b7587b437238df6e31178d
mcddna <mcddna@gmail.com>
2024-10-06 17:11:14 -0600

    c

---
+++ test_script.R
@@ -0,0 +1,18 @@
+# test script file test_script.R
+
+usethis::use_github()
+
+gitcreds::gitcreds_set()
+usethis::gh_token_help()
+
+usethis::create_github_token()
+<<<<<<< HEAD
+ 
+# update this file
+=======
+
+# update this file
+
+# update on Github!
+>>>>>>> 01d37c9a8a01d32606bf91ab9424da6ace6114f8
+xzcZXcz
